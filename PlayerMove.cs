0bc5ea2a41b93d989506b555e5e2b5727370e7f2
sohol1324 <sohol1324@users.noreply.github.com>
2019-05-16 01:49:28 +0900

    Create PlayerMove.cs

---
+++ PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float PlayerMoveSpeed;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * PlayerMoveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        anim.SetFloat("PlayerMoveX", Input.GetAxisRaw("Horizontal")); 
+    }
+}
